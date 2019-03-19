815cdd16024f7009bada509b358453f63b1ff2af
railken <railken@protonmail.ch>
2019-03-19 17:13:43 +0100

    update relations

--- src/Models/FtpAction.php
+++ src/Models/FtpAction.php
@@ -3,6 +3,8 @@
 namespace Railken\Amethyst\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Railken\Amethyst\Common\ConfigurableModel;
 use Railken\Lem\Contracts\EntityContract;
@@ -25,7 +27,7 @@ class FtpAction extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function ftp()
+    public function ftp(): BelongsTo
     {
         return $this->belongsTo(Ftp::class);
     }
@@ -33,7 +35,7 @@ class FtpAction extends Model implements EntityContract
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function data_builder()
+    public function data_builder(): BelongsTo
     {
         return $this->belongsTo(DataBuilder::class);
     }
